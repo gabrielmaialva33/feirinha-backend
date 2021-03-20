4d8b5c942bbc191992dcbfe078da20f74d430b71
Gabriel Maia <gabrielmaialva33@gmail.com>
2021-03-20 16:43:47 -0300

    chore: add base queries migration

---
+++ database/migrations/1616269350084_base_queries.ts
@@ -0,0 +1,12 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class BaseQueries extends BaseSchema {
+  public async up() {
+    await this.db.rawQuery('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";').knexQuery
+    await this.db.rawQuery('SET TIMEZONE="America/Sao_Paulo";').knexQuery
+  }
+
+  public async down() {
+    await this.db.rawQuery('DROP EXTENSION "uuid-ossp";').knexQuery
+  }
+}
