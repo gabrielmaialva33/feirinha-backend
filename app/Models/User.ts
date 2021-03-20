3a2931d26d8f49774d1d6b458e18170da707da9e
Gabriel Maia <gabrielmaialva33@gmail.com>
2021-03-20 17:50:02 -0300

    feature: add user model and hook

---
+++ app/Models/User.ts
@@ -0,0 +1,87 @@
+import { DateTime } from 'luxon'
+import {
+  afterCreate,
+  BaseModel,
+  beforeFind,
+  beforeSave,
+  column,
+  ManyToMany,
+  manyToMany,
+  ModelQueryBuilderContract,
+} from '@ioc:Adonis/Lucid/Orm'
+
+import Role from 'App/Models/Role'
+import Hash from '@ioc:Adonis/Core/Hash'
+import Database from '@ioc:Adonis/Lucid/Database'
+
+export default class User extends BaseModel {
+  @column({ isPrimary: true })
+  public id: string
+
+  @column({})
+  public name: string
+
+  @column({})
+  public username: string
+
+  @column({})
+  public email: string
+
+  @column({ serializeAs: null })
+  public password_hash: string
+
+  @column({ serializeAs: null })
+  public role_id: string
+
+  @column({ serializeAs: null })
+  public registration_completed: boolean = false
+
+  @column({ serializeAs: null })
+  public is_activated: boolean = true
+
+  @column({ serializeAs: null })
+  public is_deleted: boolean = false
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+
+  /* ----------------------------- RELATIONSHIPS ----------------------------- */
+
+  @manyToMany(() => Role, {
+    localKey: 'id',
+    pivotForeignKey: 'user_id',
+    relatedKey: 'id',
+    pivotRelatedForeignKey: 'role_id',
+    pivotTable: 'roles_users',
+  })
+  public roles: ManyToMany<typeof Role>
+
+  /* --------------------------------- HOOKS --------------------------------- */
+
+  public static async hashPassword(user: User): Promise<void> {
+    if (user.$dirty.password) {
+      user.password_hash = await Hash.make(user.password_hash)
+    }
+  }
+
+  @beforeSave()
+  public static async setUserRoleAsDefault(user: User): Promise<void> {
+    if (user.$dirty.role_id === undefined && user.$dirty.email !== 'yazo@email.com') {
+      const [{ id }] = await Database.query().select('id').from('roles').where('name', 'user')
+      user.role_id = id
+    }
+  }
+
+  @beforeFind()
+  public static async ignoreDeleted(query: ModelQueryBuilderContract<typeof User>): Promise<void> {
+    query.whereNot('is_deleted', true)
+  }
+
+  @afterCreate()
+  public static async attachRole(user: User): Promise<void> {
+    await user.related('roles').attach([user.role_id])
+  }
+}
