9da560c7ccf1887036b38801e31cf60719713a8b
Gabriel Maia <gabrielmaialva33@gmail.com>
2021-03-20 16:51:47 -0300

    feature: create users migration

---
+++ database/migrations/1616269755799_users.ts
@@ -0,0 +1,22 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Users extends BaseSchema {
+  protected tableName = 'users'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.uuid('id').primary().defaultTo(this.db.rawQuery('uuid_generate_v4()').knexQuery)
+
+      table.string('name').notNullable()
+      table.string('username').notNullable()
+      table.string('email').notNullable()
+
+      table.boolean('is_deleted').notNullable().defaultTo(false)
+      table.timestamps(true)
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
